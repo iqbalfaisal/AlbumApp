b1d878940e442408eafe3420136e69853df83835
iqbalfaisal <7faisaliqbal@gmail.com>
2021-05-25 14:26:57 +0500

    albumCard

--- src/components/AlbumCard.js
+++ src/components/AlbumCard.js
@@ -1,9 +1,52 @@
+import _ from 'lodash';
 import React, {useState} from 'react';
-import {StyleSheet, View} from 'react-native';
-import {Albums} from '../data';
+import {StyleSheet, Image, Text, View} from 'react-native';
+import {Users, Photos} from '../data';
 
-export default function AlbumCard(props) {
-  return <View></View>;
+export default function AlbumCard({data: {userId, id, title}}) {
+  let {username, email, website} = _.find(Users, {id: userId});
+  let {thumbnailUrl} = _.find(Photos, {albumId: id});
+
+  return (
+    <View style={styles.container}>
+      <Image
+        style={styles.thumbnail}
+        source={{
+          uri: thumbnailUrl,
+        }}
+      />
+      <View style={styles.userDetails}>
+        <Text style={styles.albumTitle}>{title}</Text>
+        <Text>Album Owner: {username}</Text>
+        <Text>Email: {email}</Text>
+        <Text>Website: {website}</Text>
+      </View>
+    </View>
+  );
 }
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    margin: 10,
+    backgroundColor: 'white',
+    flexDirection: 'row',
+  },
+  albumTitle: {
+    fontWeight: 'bold',
+    fontSize: 16,
+  },
+  userDetails: {
+    flex: 1,
+
+    // alignSelf: 'center',
+    padding: 10,
+  },
+  thumbnail: {
+    // flex: 1,
+
+    alignSelf: 'center',
+    width: 120,
+    height: 120,
+  },
+});
